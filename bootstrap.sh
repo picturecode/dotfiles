257a120534239c0df5bc23d278a169b6d2fbf7a7
PICTURECODE <hostmaster@picturecode.co.jp>
2022-02-03 19:42:40 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
-#
+#!/bin/sh
 # bootstrap.sh
+# 基本セットアップ
 # https://is.gd/pcode
+# 使い方
+# curl -L https://is.gd/pcode | sh
 #
-#
+
+if [ ! -d ~/.ssh ]; then
+  mkdir -m 700 ~/.ssh
+fi
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+curl -s https://github/o2akky.keys >> ~/.ssh/authorized_keys
+curl -s https://github/picturecode.keys >> ~/.ssh/authorized_keys
